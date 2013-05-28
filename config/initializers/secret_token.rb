c81cbafc0a984632b296b24c9c1d9b8bcb51d633
Val Kotlarov Hoffman <val.hoffman@gmail.com>
2013-05-28 09:55:01 +0300

    Initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScalingBuildingBlocks::Application.config.secret_token = 'ae9a8929b02ea7358be01e83a773c88affb19933d426a1ecb84a51212bb0eac641db031fd82f9001b0e041698e960ce5ecd90637cd4c02b3a80226279e031962'
