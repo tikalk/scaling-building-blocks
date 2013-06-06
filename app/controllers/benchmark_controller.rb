66d499c182a4dbbf77cd4d95d9e4a36aeb0a3331
Val Kotlarov Hoffman <val.hoffman@gmail.com>
2013-06-06 15:47:41 +0300

    Benchmark random galery page requests

--- app/controllers/benchmark_controller.rb
+++ app/controllers/benchmark_controller.rb
@@ -1,4 +1,30 @@
+require 'benchmark'
+
 class BenchmarkController < ApplicationController
-  def fetch_products_randomly
+
+  # Measure simulated random-page galery requests
+  # IN: number of page requests
+  #
+  # To be used from console only: 
+  #  c = BenchmarkController.new
+  #  c.fetch_products_randomly(100)
+  #
+  def fetch_products_randomly(num_requests = 10)
+  	items_perpage = 30
+  	pages_num = Product.count / items_perpage
+  	
+  	Benchmark.measure do 
+  		for i in 1..num_requests
+	  		random_page_num = Random.rand(1..pages_num)
+		  	@products = Product.where("id > ?", 0).reverse_order().page(random_page_num).per(items_perpage)
+		  	
+		  	# That's basicallhy what's rendered in view, so let the ticks count
+		  	@products.each do |product|
+			  	nl = product.likes.count
+			    al = "%.1f" % (product.likes.sum(&:likeability).to_f / product.likes.count) if product.likes.count.to_f != 0
+			    nr = product.reviews.count
+			end
+		end
+	end
   end
 end
