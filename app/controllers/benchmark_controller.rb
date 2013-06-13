767e5bbd1cf7412bf211fa26d155dbdf08ed2ec0
Val Kotlarov Hoffman <val.hoffman@gmail.com>
2013-06-13 10:47:02 +0300

    Benchmarks updated

--- app/controllers/benchmark_controller.rb
+++ app/controllers/benchmark_controller.rb
@@ -12,30 +12,22 @@ class BenchmarkController < ApplicationController
   #  c.fetch_products_randomly(100)
   #
   def fetch_products_randomly(num_requests = 10)
-  	items_perpage = 30
-  	pages_num = Rails.cache.fetch("pages-num" , :expires_in => 10.minutes) do
-  		Product.count / items_perpage
-  	end
-  	
-  	Benchmark.measure do 
-  		for i in 1..num_requests
-	  		random_page_num = i #Random.rand(1..pages_num)
+    items_perpage = 30
+    pages_num = Product.count / items_perpage
+    
+    Benchmark.measure do 
+      for i in 1..num_requests
+        random_page_num = Random.rand(1..pages_num)
 
-	  		# Rails.cache.fetch won't work here
-	  		@products = Rails.cache.read("products-cache-#{random_page_num}")
-	  		#binding.pry
-		  	if @products.nil?
-		  		@products = Product.where("id > ?", 0).reverse_order().page(random_page_num).per(items_perpage)	
-		  		Rails.cache.write("products-cache-#{random_page_num}", @products.all, :expires_in => 1.hour)
-		  	end
-		  	
-		  	# That's basicallhy what's rendered in view, so let the ticks count
-		  	@products.each do |product|
-			  	nl = product.likes.count
-			    al = "%.1f" % (product.likes.sum(&:likeability).to_f / product.likes.count)
-			    nr = product.reviews.count
-			end
-		end
-	end
+        @products = Product.where("id > ?", 0).reverse_order().page(random_page_num).per(items_perpage) 
+        
+        # That's basicallhy what's rendered in view, so let the ticks count
+        @products.each do |product|
+          nl = product.likes.count
+          al = "%.1f" % (product.likes.sum(&:likeability).to_f / product.likes.count)
+          nr = product.reviews.count
+        end
+      end
+    end
   end
 end
