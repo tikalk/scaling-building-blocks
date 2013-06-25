7afdd581703c403f416c10b913b438ae23ed988e
Val Kotlarov Hoffman <val.hoffman@gmail.com>
2013-06-25 14:23:41 +0300

    Done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,9 +12,9 @@ Pinukimmm::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
 
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
   #config.cache_store = :dalli_store, { :namespace => 'pinukimmm', :expires_in => 1.day, :compress => true }
-  #config.cache_store = :file_store, "#{Rails.root}/tmp/cache"
+  config.cache_store = :file_store, "#{Rails.root}/tmp/cache"
   #config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
 
   # Don't care if the mailer can't send
